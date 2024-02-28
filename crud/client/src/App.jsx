ed0b83c8e1933762989398ae5fc5c3a63976f2c3
Amsharaj <aamsharaj@gmail.com>
2024-02-28 12:42:32 +0530

    second commit

---
+++ crud/client/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './Users'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>VITE CRUD APP</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users/>}></Route>
+        <Route path='/create' element={<CreateUser/>}></Route>
+        <Route path='/update/:id' element={<UpdateUser/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
